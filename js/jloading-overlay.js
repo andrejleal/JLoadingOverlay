cb03188b239a43493b309111ea39291821010755
andrejleal <andrejleal@gmail.com>
2014-05-23 15:00:35 +0100

    Fullscreen or inside a container mode

--- js/jloading-overlay.js
+++ js/jloading-overlay.js
@@ -2,20 +2,42 @@
 ///Author: André Leal & Pedro Sombreireiro
 /////////////////////////////////////////////////////
 
+
 (function ($) {
     var jLoadingOverlayId = 'jLoadingOverlay';
+    var jLoadingOptions;
+     $.fn.jLoadingOverlay = function (action, options) {
+         jLoadingOptions = options;
+         if (action == '') {
 
-     $.fn.jLoadingOverlay = function (action) {
-         if(action == ''){
-             $(this).append('<div id="' + jLoadingOverlayId + '" class="loading-overlay" style="display: none;"></div>');
-             $('DIV#'+jLoadingOverlayId).append('<div class="spinner"></div>');
-             $('DIV#'+jLoadingOverlayId).append('<div class="overlay-bg"></div>');
-             $('DIV#' + jLoadingOverlayId + ' DIV.spinner').append('<div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div>');
-             $('DIV#' + jLoadingOverlayId).show();
+             var container = $('body');
+
+             var wrapperClass = 'loading-wrapper';
+             if (options) {
+                 if (!options.fullscreen) {
+                     wrapperClass = wrapperClass + ' inner-pane';
+                     container = $(this);
+                 }
+             }
+
+
+             container.append('<div id="' + jLoadingOverlayId + '" class="'+wrapperClass+'" style="display: none;"></div>');
+             $('div#' + jLoadingOverlayId).append('<div class="loading-overlay"></div>');
+             $('div#' + jLoadingOverlayId + ' div.loading-overlay').append('<div class="spinner"></div>');
+             $('div#' + jLoadingOverlayId).append('<div class="overlay-bg"></div>');
+             $('div#' + jLoadingOverlayId + ' div.spinner').append('<div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div>');
+			 
+			 if(options && options.backgroundColor){
+				$('div#' + jLoadingOverlayId +' div.overlay-bg').css('background',options.backgroundColor);
+			 }
+			 
+			 
+			 
+             $('div#' + jLoadingOverlayId).show();
          }
          else if('close'){
-             $('DIV#'+jLoadingOverlayId).detach();
+             $('div#' + jLoadingOverlayId).detach();
          }
-		 return true;
      }
  })(jQuery);
+
